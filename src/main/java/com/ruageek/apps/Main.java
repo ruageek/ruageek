7e1414d9e7e23640589cda5675e34475fb088a30
Thomas Leplus <thomasleplus@users.noreply.github.com>
2022-09-05 22:15:12 -0700

    Test

---
+++ src/main/java/com/ruageek/apps/Main.java
@@ -0,0 +1,8 @@
+package com.ruageek.apps;
+
+public class Main {
+
+    public static void main(String[] args) {
+    }
+    
+}
---
+++ src/test/java/com/ruageek/apps/TestMain.java
@@ -0,0 +1,20 @@
+package com.ruageek.apps;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * 
+ */
+
+/**
+ * @author thomas
+ *
+ */
+class TestMain {
+
+    @Test
+    void test() {
+        Main.main(null);
+    }
+
+}
