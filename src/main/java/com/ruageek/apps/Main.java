7912c4041bf32f23d8c4f2db3f9be19772f0fb63
Thomas Leplus <thomasleplus@users.noreply.github.com>
2024-04-08 19:12:20 -0300

    Reformat

--- src/main/java/com/ruageek/apps/Main.java
+++ src/main/java/com/ruageek/apps/Main.java
@@ -14,5 +14,7 @@ public final class Main {
    *
    * @param args The command line arguments.
    */
-  public static void main(final String[] args) {}
+  public static void main(final String[] args) {
+    return;
+  }
 }
